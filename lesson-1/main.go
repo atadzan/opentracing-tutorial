0438a0f5ef27fc1220e7e4e86fbd11fc28f7d21b
atadzan <envader001@gmail.com>
2023-10-10 16:16:00 +0500

    init & lesson-1

---
+++ lesson-1/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/config"
+	"io"
+)
+
+func initJaeger(service string) (opentracing.Tracer, io.Closer) {
+	cfg := &config.Configuration{
+		ServiceName: service,
+		Sampler: &config.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &config.ReporterConfig{
+			LogSpans: true,
+		},
+	}
+	tracer, closer, err := cfg.NewTracer(config.Logger(jaeger.StdLogger))
+	if err != nil {
+		panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
+	}
+	return tracer, closer
+}
+
+func main() {
+	tracer, closer := initJaeger("hello-world")
+	defer closer.Close()
+
+	span := tracer.StartSpan("print Hello")
+	println("hello")
+	span.Finish()
+}
